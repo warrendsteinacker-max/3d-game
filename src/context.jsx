901b5b56de135add9a55ef5152e68f22fb2bf595
Warren <warrendsteinacker@gmail.com>
2026-01-03 11:39:22 -0500

    creating needed things for landing page

--- src/context.jsx
+++ src/context.jsx
@@ -1,9 +1,18 @@
 import { createContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 export const DatC = createContext()
 
 export const DataP = ({childre}) => {
+
+    ///////needed things for landing page
+    const N = useNavigate()
+    const go2game = () => {
+        N("/G")
+    }
+
+    ///////
 
 
     return(<DatC.Provider value={{}}>{childre}</DatC.Provider>)
