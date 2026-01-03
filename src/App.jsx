6be89d28924f0324b463a4491e8c02ffbfa3e4c5
Warren <warrendsteinacker@gmail.com>
2026-01-03 12:12:26 -0500

    think I made final fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { DataP } from "./context"
-
+///import { DataP } from "./context"
+import LP from "./componets/landingp"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <DataP>
+
         <BrowserRouter>
         <Routes>
-          <Route path="/" element={}></Route>
-          <Route path="/G"></Route>
-          <Route path="/edit"></Route>
+          <Route path="/" element={<LP/>}></Route>
         </Routes>
         </BrowserRouter>
-      </DataP>
+
     </>
   )
 }
 
 export default App
+
+/////          <Route path="/G"></Route>
+////          <Route path="/edit"></Route>
--- src/componets/landingp.jsx
+++ src/componets/landingp.jsx
@@ -1,18 +1,37 @@
-import React, {} from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
-
 const LP = () => {
+    return (
+        <div style={{ position: "relative", height: "100vh", width: "100vw", overflow: "hidden" }}>
+            {/* Background Image */}
+            <img 
+                src="/vimal-s-GBg3jyGS-Ug-unsplash.jpg" 
+                style={{ height: "100vh", width: "100vw", objectFit: "cover" }} 
+                alt="Space Background"
+            />
 
-
-
-return (
-    <>
-        <img src="\public\vimal-s-GBg3jyGS-Ug-unsplash.jpg" style={{height: "100vh"}}></img>
-        <h1 style={{color: "green"}}>Welcome to Space Game</h1>
-        <Link>Start Game</Link>
-    </>
-)
+            {/* Content Overlay */}
+            <div style={{ 
+                position: "absolute", 
+                top: "50%", 
+                left: "50%", 
+                transform: "translate(-50%, -50%)", 
+                textAlign: "center" 
+            }}>
+                <h1 style={{ color: "green", fontSize: "3rem" }}>Welcome to Space Game</h1>
+                <Link to="/G" style={{ 
+                    color: "white", 
+                    background: "green", 
+                    padding: "10px 20px", 
+                    textDecoration: "none",
+                    borderRadius: "5px"
+                }}>
+                    Start Game
+                </Link>
+            </div>
+        </div>
+    );
 }
 
-export default LP
+export default LP;
