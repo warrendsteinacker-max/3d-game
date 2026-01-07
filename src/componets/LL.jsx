8c2658e9866d9e7dec5c44474a4976c1db6bb5ea
Warren <warrendsteinacker@gmail.com>
2026-01-07 15:46:45 -0500

    fixing centering problem

--- src/componets/LL.jsx
+++ src/componets/LL.jsx
@@ -2,5 +2,5 @@
 import {Link} from "react-router-dom"
 
 export const LL = () => {
-    return(<><div style={{backgroundColor: "blue"}}><nav><Link to="/G" style={{}}>game</Link><Link to="/">landing page</Link><Link to="/map">map</Link></nav></div></>)
+    return(<><div style={{borderRadius: "5px", backgroundColor: "blue", width: "500px", display: "flex", alignItems: "center", justifyContent: "center"}}><nav><Link to="/G" style={{color: "red", textDecoration: "none", borderRadius: "5px", marginLeft: "10px"}}>game</Link><Link to="/" style={{color: "red", textDecoration: "none", borderRadius: "5px", marginLeft: "10px"}}>landing page</Link><Link to="/map" style={{color: "red", textDecoration: "none", borderRadius: "5px", marginLeft: "10px"}}>map</Link></nav></div></>)
 }
