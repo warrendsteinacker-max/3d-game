33ad22c5f0df17e5a4c5c6f05e57972d91838cca
Warren <warrendsteinacker@gmail.com>
2026-01-07 14:36:14 -0500

    working on comp

--- src/componets/LL.jsx
+++ src/componets/LL.jsx
@@ -1,5 +1,5 @@
 
 const LL = () => {
 
-    return()
+    return(<><header><img src="./public/earth_clouds.jpg" style={{width: "50px", height: "50px"}}/></header><main></main><footer></footer></>)
 }
