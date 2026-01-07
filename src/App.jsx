06f432cdc992f300a8019917ac7349eda04311be
Warren <warrendsteinacker@gmail.com>
2026-01-07 16:06:22 -0500

    adding nav bar to applications

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { DatProvider } from "./context"; // Adjust this path to where your context.jsx is
 import LP from "./componets/landingp";
 import Gamep from "./componets/gamep";
@@ -9,6 +9,9 @@ function App() {
   return (
     <DatProvider> {/* THIS MUST WRAP EVERYTHING */}
       <BrowserRouter>
+        <nav>
+          <Link to="/">landing page</Link>
+        </nav>
         <Routes>
           <Route path="/" element={<LP />} />
           <Route path="/LL" element={<LL/>}/>
