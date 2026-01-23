f6b41b2b82d30ee9e4a644dec79369c425f20085
Warren <warrendsteinacker@gmail.com>
2026-01-23 12:05:48 -0500

    practice

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,24 @@ import {LL} from "./componets/LL";
 import NP from "./componets/NP";
 
 function App() {
-  return (
-    <DatProvider> {/* THIS MUST WRAP EVERYTHING */}
-      <BrowserRouter>
-        <nav>
-          <Link to="/">landing page</Link>
-          <Link to="/np">new p</Link>
-        </nav>
-        <Routes>
-          <Route to="/np" element={<NP/>}></Route>
-          <Route path="/" element={<LP />} />
-          <Route path="/LL" element={<LL/>}/>
-          <Route path="/G" element={<Gamep />} />
-          <Route path="/map" element={<PlanetMap />} />
-        </Routes>
-      </BrowserRouter>
-    </DatProvider>
-  );
+  // return (
+  //   <DatProvider> {/* THIS MUST WRAP EVERYTHING */}
+  //     <BrowserRouter>
+  //       <nav>
+  //         <Link to="/">landing page</Link>
+  //         <Link to="/np">new p</Link>
+  //       </nav>
+  //       <Routes>
+  //         <Route to="/np" element={<NP/>}></Route>
+  //         <Route path="/" element={<LP />} />
+  //         <Route path="/LL" element={<LL/>}/>
+  //         <Route path="/G" element={<Gamep />} />
+  //         <Route path="/map" element={<PlanetMap />} />
+  //       </Routes>
+  //     </BrowserRouter>
+  //   </DatProvider>
+  // );
+  return()
 }
 
 export default App;
